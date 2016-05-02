5adb6f793c90031a05dff637f5f7a57d189c51c1
James Earle <j_earle@hotmail.com>
2016-05-02 16:26:21 -0400

    fixed scroll, code cleanup with auto-formatter

--- js/script-old.js
+++ js/script-old.js
@@ -1,43 +1,43 @@
-$.fn.exBounce = function(){
+$.fn.exBounce = function () {
   var self = this;
-  (function runEffect(){
-    self.effect("bounce", { times:1 }, 800, runEffect);
+  (function runEffect() {
+    self.effect("bounce", { times: 1 }, 800, runEffect);
   })();
-      
+
   return this;
 };
 
-$(window).load(function() {
+$(window).load(function () {
   $("#arrow").exBounce();
-  
-  $('a').click(function(){
+
+  $('a').click(function () {
     $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
+			scrollTop: $($.attr(this, 'href')).offset().top
     }, 600);
     return false;
   });
-	
-	$("#b1").click(function() {
-	  $(".popover").hide();
+
+	$("#b1").click(function () {
+		$(".popover").hide();
 		$("#p1").show();
 	});
-	
-	$("#b2").click(function() {
-	  $(".popover").hide();
+
+	$("#b2").click(function () {
+		$(".popover").hide();
 		$("#p2").show();
 	});
-	
-	$("#b3").click(function() {
-	  $(".popover").hide();
+
+	$("#b3").click(function () {
+		$(".popover").hide();
 		$("#p3").show();
 	});
-	
-	$("#b4").click(function() {
-	  $(".popover").hide();
+
+	$("#b4").click(function () {
+		$(".popover").hide();
 		$("#p4").show();
 	});
-	
-	$(".popover").click(function() {
+
+	$(".popover").click(function () {
 		$(".popover").hide();
 	});
 });
